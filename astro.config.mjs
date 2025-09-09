2ee8679fda45c4b0767c6835da5a74d360a72425
Andres Adjimann <aadjiman@gmail.com>
2025-09-09 14:15:19 -0300

    chore: first attempt to add pdf

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,18 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import {defineConfig} from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
+import pdf from 'astro-pdf'; // Import the integration
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://a-d-ji.github.io',
     base: '/cv',
+    integrations: [pdf({
+        pages: {"/cv/text": "cv.pdf"},
+        launch: {args: ['--no-sandbox']}
+    })],
     vite: {
-    plugins: [tailwindcss()]
-  }
+        plugins: [tailwindcss()]
+    }
 });
