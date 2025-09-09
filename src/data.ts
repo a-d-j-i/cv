b8db20c2382830ab877737e8769876cac50d309e
Andres Adjimann <aadjiman@gmail.com>
2025-09-08 21:28:52 -0300

    feat: initial import

---
+++ src/data.ts
@@ -0,0 +1,18 @@
+export const data = {
+    name: "Andrés Adjimann",
+    jobTitle: "Security-focused Software Engineer",
+    city: "CABA",
+    country: "Argentina",
+    email: "aadjiman@gmail.com",
+    linkedinProfile: "https://www.linkedin.com/in/andresadjimann/",
+    telegram: "https://t.me/a_d_j_i",
+    telegramUser: "@adji",
+    githubProfiles: [{
+        name: "a-d-j-i",
+        url: "https://github.com/a-d-j-i"
+    },
+        {
+            name: "adjisb",
+            url: "https://github.com/adjisb",
+        }]
+}
