fe3a8682cf397a866199b1507fbd9d24710d8df1
Andres Adjimann <aadjiman@gmail.com>
2025-09-09 08:18:16 -0300

    chore: github pages build

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://a-d-ji.github.io',
+    base: '/cv',
     vite: {
     plugins: [tailwindcss()]
   }
